31f5083fc4a3476d50db64a91639fcdd2617e8f8
chie0505 <adtnn.50@gmail.com>
2021-07-17 10:23:39 +0000

    Initial Commit1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,83 @@
 class BooksController < ApplicationController
 
+
+
 def index
   @books = Book.all
   @book = Book.new
+  # @book = Book.find(params[:id])
 end
 
- def create
-  # １. データを新規登録するためのインスタンス作成
-  @book = Book.new(book_params)
-  # ２. データをデータベースに保存するためのsaveメソッド実行@は画面に表示させる必要
-  @book.save
-  # ３. トップ画面へリダイレクト＊後で変更
-  redirect_to '/books'
- end
+def show
+  @book = Book.find(params[:id])
+end
 
- def show
+# def create
+
+#   # １. データを新規登録するためのインスタンス作成
+#   @book = Book.new(book_params)
+#   # ２. データをデータベースに保存するためのsaveメソッド実行
+#   # @は画面に表示させる必要があるものにつける。Viewと対応していること
+#   @book.save
+#   redirect_to Book.find(@book.id)
+#   # redirect_to Book.find(@book.id)
+#   flash[:notice] = "Book was successfully created."
+# end
+
+def create
+  @book = Book.new(book_params)
+  if @book.save
+    redirect_to Book.find(@book.id)
+else
+    @books = Book.all
+    render 'index'
+  end
+  flash[:notice] = "Book was successfully created."
+end
+
+  # def create
+  #     @book = Book.new(book_params)
+  #     if @book.save
+  #         redirect_to book_path(@book)
+  #     else
+  #         render :new
+  #     end
+
+
+ def edit
   @book = Book.find(params[:id])
  end
+
+
+# if @book.save
+#     redirect_to Book.find(@book.id)
+# else
+#     @books = Book.all
+#     render 'index'
+#     # render ("books/index")
+#     # render new
+#   end
+
+
+
+ def update
+  @book = Book.find(params[:id])
+   if @book.update(book_params)
+   redirect_to Book.find(params[:id])
+    else
+     render 'edit'
+    end
+  flash[:notice] = "Book was successfully updated."
+ end
+
+
+def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to books_path
+  flash[:notice] = "Book was successfully destroyed."
+end
+
 
   private
   # ストロングパラメータ(下段に書く)
--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_07_11_031506) do
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.string "category"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
